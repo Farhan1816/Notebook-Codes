2aa9365732448a87724042f05e330052b45d538d
Inori <103218515+Farhan1816@users.noreply.github.com>
2025-11-13 23:59:39 +0600

    Add FFT and convolution functions in FFTMod.cpp

---
+++ Math/FFTMod.cpp
@@ -0,0 +1,64 @@
+typedef long double ld; // USE DOUBLE, MUCH FASTER AND ALMOST ALWAYS WORKS
+
+typedef complex<double> C;
+typedef vector<double> vd;
+typedef vector<int>vi;
+
+#define sz(a) a.size()
+#define rep(i, j, k) for(int i=j;i<k;i++)
+
+void fft(vector<C>& a) {
+	int n = sz(a), L = 31 - __builtin_clz(n);
+	static vector<complex<long double>> R(2, 1);
+	static vector<C> rt(2, 1);  // (^ 10% faster if double)
+	for (static int k = 2; k < n; k *= 2) {
+		R.resize(n); rt.resize(n);
+		auto x = polar(1.0L, acos(-1.0L) / k);
+		rep(i,k,2*k) rt[i] = R[i] = i&1 ? R[i/2] * x : R[i/2];
+	}
+	vi rev(n);
+	rep(i,0,n) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
+	rep(i,0,n) if (i < rev[i]) swap(a[i], a[rev[i]]);
+	for (int k = 1; k < n; k *= 2)
+		for (int i = 0; i < n; i += 2 * k) rep(j,0,k) {
+			// C z = rt[j+k] * a[i+j+k]; // (25% faster if hand-rolled)  /// include-line
+			auto x = (double *)&rt[j+k], y = (double *)&a[i+j+k];        /// exclude-line
+			C z(x[0]*y[0] - x[1]*y[1], x[0]*y[1] + x[1]*y[0]);           /// exclude-line
+			a[i + j + k] = a[i + j] - z;
+			a[i + j] += z;
+		}
+}
+
+typedef vector<ll> vl;
+template<int M> vl convMod(const vl &a, const vl &b) {
+	if (a.empty() || b.empty()) return {};
+	vl res(sz(a) + sz(b) - 1);
+	int B=32-__builtin_clz(sz(res)), n=1<<B, cut=int(sqrt(M));
+	vector<C> L(n), R(n), outs(n), outl(n);
+	for(int i=0;i<(int)a.size();i++) L[i] = C((int)a[i] / cut, (int)a[i] % cut);
+	for(int i=0;i<(int)b.size();i++) R[i] = C((int)b[i] / cut, (int)b[i] % cut);
+	fft(L), fft(R);
+	for(int i=0;i<n;i++) {
+		int j = -i & (n - 1);
+		outl[j] = (L[i] + conj(L[j])) * R[i] / (2.0 * n);
+		outs[j] = (L[i] - conj(L[j])) * R[i] / (2.0 * n) / 1i;
+	}
+	fft(outl), fft(outs);
+	for(int i=0;i<(int)res.size();i++) {
+		ll av = ll(real(outl[i])+.5), cv = ll(imag(outs[i])+.5);
+		ll bv = ll(imag(outl[i])+.5) + ll(real(outs[i])+.5);
+		res[i] = ((av % M * cut + bv) % M * cut + cv) % M;
+	}
+	return res;
+}
+
+vector<ll>find_all_sums(int st, int en, vector<vector<ll>>&v)
+{
+  if(st==en)return v[st];
+  
+  int mid = (st+en)/2;
+  vector<ll>ans1 = find_all_sums(st, mid, v);
+  vector<ll>ans2 = find_all_sums(mid+1, en, v);
+  vector<ll> ans = convMod<786433>(ans1, ans2);
+  return ans;
+}
