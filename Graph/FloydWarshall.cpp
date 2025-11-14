36414447940a8b15910aa9fe6ee1798c3ff222d3
Inori <103218515+Farhan1816@users.noreply.github.com>
2025-11-14 17:43:18 +0600

    Add Floyd-Warshall algorithm implementation
    Implement Floyd-Warshall algorithm for shortest paths.

---
+++ Graph/FloydWarshall.cpp
@@ -0,0 +1,9 @@
+//can have negative weight, but no negative cycle.
+for (int k = 0; k < n; ++k) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (d[i][k] < INF && d[k][j] < INF)
+                d[i][j] = min(d[i][j], d[i][k] + d[k][j]); 
+        }
+    }
+}
