ce843ef4d00ed4d2134f020d25334dd5a008e8d4
Inori <103218515+Farhan1816@users.noreply.github.com>
2025-11-14 17:45:48 +0600

    Add Bellman-Ford algorithm implementation
    Implement Bellman-Ford algorithm for single source shortest path.

---
+++ Graph/Bellman Ford.cpp
@@ -0,0 +1,16 @@
+//single source shortest path, can have negative weight, but no negatice cycle.
+vector<int> d(n, INF);
+    d[v] = 0;
+    for (;;) {
+        bool any = false;
+
+        for (Edge e : edges)
+            if (d[e.a] < INF)
+                if (d[e.b] > d[e.a] + e.cost) {
+                    d[e.b] = d[e.a] + e.cost;
+                    any = true;
+                }
+
+        if (!any)
+            break;
+    }
