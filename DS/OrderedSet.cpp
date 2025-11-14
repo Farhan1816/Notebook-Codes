e4098b2f7622828d5b56b0eb38c77219275fcf24
Inori <103218515+Farhan1816@users.noreply.github.com>
2025-11-14 12:38:03 +0600

    Add OrderedSet implementation using PBDS

---
+++ DS/OrderedSet.cpp
@@ -0,0 +1,13 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,
+    tree_order_statistics_node_update>;
+// find_by_order(k) : iterator to the k'th element (0-indexed)
+// order_of_key(k) : number of items strictly smaller than k
+// auto cmp = [](int a, int b) { return a < b; };
+// tree<int, null_type, decltype(cmp)> x(cmp);
+// tree<int, null_type, function<bool(int, int)>> y(
+//     [](int a, int b) { return a < b; });
+//ordered_set<int>os
