334e3a6e9b062ddbce6e6b778f77cd40cd8c9fa0
Inori <103218515+Farhan1816@users.noreply.github.com>
2025-11-15 09:33:44 +0600

    Refactor Convex Hull Trick implementation

--- DP/CHT.cpp
+++ DP/CHT.cpp
@@ -1,49 +1,43 @@
-//https://atcoder.jp/contests/dp/tasks/dp_z
-struct Line {
-	mutable ll k, m, p;
-	bool operator<(const Line& o) const { return k < o.k; }
-	bool operator<(ll x) const { return p < x; }
+//add lines with -m and -b and return -ans to
+//make this code work for minimums.(not -x)
+const ll inf = -(1LL << 62);
+struct line {
+  ll m, b;
+  mutable function<const line*() > succ;
+  bool operator < (const line& rhs) const {
+    if (rhs.b != inf) return m < rhs.m;
+    const line* s = succ();
+    if (!s) return 0;
+    ll x = rhs.m;
+    return b - s->b < (s->m - m) * x;
+  }
 };
-
-struct LineContainer : multiset<Line, less<>> {
-	static const ll inf = LLONG_MAX;
-	ll div(ll a, ll b) { // floored division
-		return a / b - ((a ^ b) < 0 && a % b);
-	}
-	bool isect(iterator x, iterator y) {
-		if (y == end()) return x->p = inf, 0;
-		if (x->k == y->k) x->p = x->m > y->m ? inf : -inf;
-		else x->p = div(y->m - x->m, x->k - y->k);
-		return x->p >= y->p;
-	}
-	void add(ll k, ll m) { // Insert lines in negated form
-		auto z = insert({-k, -m, 0}), y = z++, x = y;
-		while (isect(y, z)) z = erase(z);
-		if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
-		while ((y = x) != begin() && (--x)->p >= y->p)
-			isect(x, erase(y));
-	}
-	ll query(ll x) { 
-		assert(!empty());
-		auto l = *lower_bound(x);
-		return -(l.k * x + l.m);
-	}
+struct CHT : public multiset<line> {
+  bool bad(iterator y) {
+    auto z = next(y);
+    if (y == begin()) {
+      if (z == end()) return 0;
+      return y -> m == z -> m && y -> b <= z -> b;
+    }
+    auto x = prev(y);
+    if (z == end()) return y -> m == x -> m && y -> b <= x -> b;
+    return 1.0 * (x -> b - y -> b) * (z -> m - y -> m) >= 1.0 * (y -> b - z -> b) * (y -> m - x -> m);
+  }
+  void add(ll m, ll b) {
+    auto y = insert({ m, b });
+    y->succ = [=, this] { return next(y) == end() ? 0 : &*next(y); };
+    if (bad(y)) {
+      erase(y);
+      return;
+    }
+    while (next(y) != end() && bad(next(y))) erase(next(y));
+    while (y != begin() && bad(prev(y))) erase(prev(y));
+  }
+  ll query(ll x) {
+    assert(!empty());
+    auto l = *lower_bound((line) {
+      x, inf
+    });
+    return l.m * x + l.b;
+  }
 };
-
-void solve()
-{
-	int n, c;
-	cin >>n >> c;
-	int arr[n+1], dp[n+1];
-	for(int i=1;i<=n;i++)cin >> arr[i];
-	dp[1] = 0;
-	LineContainer lc;
-	lc.add(-2*arr[1], dp[1]+arr[1]*arr[1]);
-	int ans = 1e18;
-	for(int i=2;i<=n;i++){
-		dp[i] = arr[i]*arr[i] + c + lc.query(arr[i]);
-		// cout << dp[i] << " ";
-		lc.add(-2*arr[i], dp[i]+arr[i]*arr[i]);
-	}
-	cout << dp[n] << "\n";
-}
